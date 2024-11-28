f55175d60267d2352cf55e01fa3987467d583e7b
Katnax <zach.po@hotmail.com>
2024-11-28 01:08:08 +0100

    first sketch of a program

--- main.go
+++ main.go
@@ -5,31 +5,24 @@ import (
 	"math/rand"
 )
 
-func main() {
+func randomazzo(priorities_sum int) (priorities_result int) {
 
-// input the strings separately. 
-//	TODO import data from the file
+	random := rand.Intn(priorities_sum)
 
+	priorities_result = random - priorities_sum
 
-
-
-
-
-
-
-
-
-
-
-
-
+	return priorities_result
 }
 
+func main() {
 
-
-
-func randomazzo {
-
-
-
+	// input the strings separately.
+	//	TODO import data from the file
+	var zadania []string = []string{}
+	for n := 0; ; n++ {
+		fmt.Scan(&zadania[n])
+		if zadania[n] == "end" {
+			break
+		}
+	}
 }
