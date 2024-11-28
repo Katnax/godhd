66e68f86f8fa920d5f6cfd12df3c1e35bb9abc26
Katnax <zach.po@hotmail.com>
2024-11-28 01:33:41 +0100

    lookin better

--- main.go
+++ main.go
@@ -18,11 +18,23 @@ func main() {
 
 	// input the strings separately.
 	//	TODO import data from the file
-	var zadania []string = []string{}
-	for n := 0; ; n++ {
-		fmt.Scan(&zadania[n])
-		if zadania[n] == "end" {
+	var jobs []string = []string{}
+	for i := 0; ; i++ {
+		fmt.Scan(&jobs[i])
+		if jobs[i] == "end" {
 			break
 		}
 	}
+	fmt.Println("set priorities! ")
+	var priorities_sum int
+	var priority int
+	for _, job := range jobs {
+
+		fmt.Print(job, " : ")
+		fmt.Scan(&priority)
+		fmt.Println()
+		priorities_sum += priority
+
+	}
+
 }
